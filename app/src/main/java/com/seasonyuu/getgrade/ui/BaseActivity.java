aab677dc15c49547d0c6d9e8fd7b2b6cec05e25e
seasonyuu <seasonyuu@gmail.com>
2015-09-09 14:18:32 +0800

    使对话框警告更易用

--- app/src/main/java/com/seasonyuu/getgrade/ui/BaseActivity.java
+++ app/src/main/java/com/seasonyuu/getgrade/ui/BaseActivity.java
@@ -1,6 +1,9 @@
 package com.seasonyuu.getgrade.ui;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -11,6 +14,8 @@ import com.seasonyuu.getgrade.app.GGApplication;
  * Created by seasonyuu on 15/9/7.
  */
 public class BaseActivity extends AppCompatActivity {
+	private ProgressDialog progressDialog;
+	private AlertDialog warningDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,36 @@ public class BaseActivity extends AppCompatActivity {
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+
+	public void showProgressDialog(String message) {
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
+//  		progressDialog.setCancelable(false);
+			progressDialog.show();
+		}
+		progressDialog.setMessage(message);
+		progressDialog.show();
+	}
+
+	public void showWarning(String message, DialogInterface.OnClickListener onClickListener) {
+		if (warningDialog == null)
+			warningDialog = new AlertDialog.Builder(this)
+					.setMessage(message)
+					.setTitle("警告")
+					.setPositiveButton("确定", onClickListener)
+					.create();
+		else {
+			warningDialog.setMessage(message);
+			warningDialog.setButton(DialogInterface.BUTTON_POSITIVE, "确定", onClickListener);
+		}
+		warningDialog.show();
+	}
+
+	public void cancelDialog() {
+		if (progressDialog != null && progressDialog.isShowing())
+			progressDialog.cancel();
+		if (warningDialog != null && warningDialog.isShowing())
+			warningDialog.cancel();
 	}
 }
