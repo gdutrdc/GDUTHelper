e5dbb573fe8b803be6a8e0cafecfb8be19a377c0
seasonyuu <seasonyuu@gmail.com>
2016-01-06 23:30:05 +0800

    添加对话框成型

--- app/src/main/java/com/rdc/gduthelper/ui/GetScheduleActivity.java
+++ app/src/main/java/com/rdc/gduthelper/ui/GetScheduleActivity.java
@@ -4,11 +4,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.rdc.gduthelper.R;
 import com.rdc.gduthelper.app.GDUTHelperApp;
 import com.rdc.gduthelper.bean.Lesson;
-import com.rdc.gduthelper.ui.widget.AddScheduleDialog;
 import com.rdc.gduthelper.utils.database.ScheduleDBHelper;
 
 import java.util.ArrayList;
@@ -17,6 +19,10 @@ import java.util.ArrayList;
  * Created by seasonyuu on 16/1/4.
  */
 public class GetScheduleActivity extends BaseActivity {
+	private AppCompatSpinner mSpinnerYear;
+	private AppCompatSpinner mSpinnerTerm;
+
+	private boolean isNull;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class GetScheduleActivity extends BaseActivity {
 		ScheduleDBHelper helper = new ScheduleDBHelper(this);
 		ArrayList<Lesson> lessons = helper.getLessonList(null);
 		if (lessons == null || lessons.size() == 0) {
+			isNull = true;
 			new AlertDialog.Builder(this)
 					.setTitle(R.string.tips)
 					.setMessage(R.string.no_local_schedule)
@@ -45,18 +52,24 @@ public class GetScheduleActivity extends BaseActivity {
 	}
 
 	private void showAddSchedule() {
-		AddScheduleDialog dialog = new AddScheduleDialog(this);
-		dialog.setOnButtonClickListener(new AddScheduleDialog.OnButtonClickListener() {
-			@Override
-			public void onEnsure(DialogInterface dialog) {
+		View view = LayoutInflater.from(this).inflate(R.layout.activity_add_schedule, null);
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.add_schedule)
+				.setView(view)
+				.setPositiveButton(R.string.ensure, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
 
-			}
+					}
+				})
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (isNull) {
+							finish();
+						}
+					}
+				}).show();
 
-			@Override
-			public void onCancel(DialogInterface dialog) {
-				finish();
-			}
-		});
-		dialog.show();
 	}
 }
